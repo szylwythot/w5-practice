fcb734e5406e903f84b9d8a51f2f33d0ce978ba4
toth_szilvia <szylwy@gmail.com>
2022-02-14 15:00:20 +0100

    log primitive types

---
+++ frontend/script.js
@@ -0,0 +1,20 @@
+// console.log("Hello!");
+// console.log('sima');
+// console.log(`backtick`);
+
+// console.log(2);
+// console.log(typeof NaN);
+// console.log(0/0);
+// console.log(10/0);
+// console.log(2.14);
+
+// console.log(typeof NaN);
+// console.log(typeof Infinity);
+
+// console.log(true);
+// console.log(typeof false);
+
+console.log(undefined);
+console.log(typeof undefined);
+console.log(null);
+console.log(typeof null);
