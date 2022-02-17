1da1d3ea91e96e76f58ffe92481dfce63c1f32b5
toth_szilvia <szylwy@gmail.com>
2022-02-17 15:47:24 +0100

    add sort

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,9 @@
-function loadEvent(){
+function loadEvent() {
 
     let rootElement = document.getElementById("root");
 
-    let card = function (title, year, rate){
-            return `
+    let card = function (title, year, rate) {
+        return `
             <div class="card">
                 <h2>${title}</h2>
                 <div class="time">${year}</div>
@@ -11,17 +11,25 @@ function loadEvent(){
             </div>
             `;
     };
+    
+    // movies.sort(function(movie1, movie2){return movie1.year - movie2.year});
 
+    let newGoodMoivies = [];
     for (const movieSend of movies) {
-        let newerThen200 = false;
-        if(movieSend.year > 2000){
-            newerThen200 = true;
-        }
-        if(newerThen200){
-            rootElement.insertAdjacentHTML("beforeend", card(movieSend.title, movieSend.year, movieSend.rate));
-        }
-    }
+        // let floorRate = Math.floor(movieSend.rate);
 
+        if (movieSend.year > 2000 && movieSend.rate >= 8) {
+            newGoodMoivies.push(movieSend);
+            // rootElement.insertAdjacentHTML("beforeend", card(movieSend.title, movieSend.year, floorRate));
+        }
+
+    }
+    
+    newGoodMoivies.sort(function(a, b){return a.year -b.year});
+    for (const movieSend of newGoodMoivies) {
+        let floorRate = Math.floor(movieSend.rate);
+        rootElement.insertAdjacentHTML("beforeend", card(movieSend.title, movieSend.year, floorRate));
+    }
     console.log(movies);
 }
 
